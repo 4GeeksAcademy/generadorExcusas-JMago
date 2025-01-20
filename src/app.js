e2928715b5d8f061a142a7aaf84370bc36889507
Jfmm1983 <123413274+Jfmm1983@users.noreply.github.com>
2025-01-20 23:38:29 +0000

    fix/app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  console.log("Hello Rigo from the console!");
+
+function randomRangeNumber(min, max){
+  
+    return Math.floor(Math.random() * (max - min) );
+}
+
+let quien= who[randomRangeNumber(0,4)];
+let accion=action[randomRangeNumber(0,4)];
+let que=what[randomRangeNumber(0,3) ];
+let cuando =when[randomRangeNumber(0,5)];
+
+let excusa = (quien+" ")+(accion+" ")+(que+" ")+(cuando+" ");
+
+
 };
